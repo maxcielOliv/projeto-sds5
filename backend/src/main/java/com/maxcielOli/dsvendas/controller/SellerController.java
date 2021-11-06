49440588f19ac5b4d99d74f2c2f30aa413966212
MaxcielOliv <maxcieloliveira@hotmail.com>
2021-11-06 15:52:31 -0300

    Layers

---
+++ backend/src/main/java/com/maxcielOli/dsvendas/controller/SellerController.java
@@ -0,0 +1,27 @@
+package com.maxcielOli.dsvendas.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.maxcielOli.dsvendas.dto.SellerDTO;
+import com.maxcielOli.dsvendas.services.SellerService;
+
+@RestController
+@RequestMapping(value = "/sellers")
+public class SellerController {
+	
+	@Autowired
+	private SellerService service;
+	
+	@GetMapping
+	public ResponseEntity<List<SellerDTO>> findALL(){
+		List<SellerDTO> list = service.findAll();
+		return ResponseEntity.ok(list);
+	}
+
+}
---
+++ backend/src/main/java/com/maxcielOli/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.maxcielOli.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.maxcielOli.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+
+}
